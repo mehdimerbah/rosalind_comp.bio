554df7a0d0003d4d8f60482ba1c69e5b2ad06d40
mehdimerbah <mehdimerbah@outlook.com>
2021-04-15 12:26:46 +0100

    Added GC content calculator

--- stronghold/computing_gc_content.py
+++ stronghold/computing_gc_content.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python3
+import sys
 
-f = open('test.txt', 'rt')
+args = sys.argv
+if len(args) == 1:
+    print("Please Specify filename")
+    sys.exit()
+
+f = open(args[1], 'rt')
+
+def get_gc():            
+    entries = {}
+    for line in f:
+        if '>' in line:
+            seq_id = line.strip()[1:]
+            entries[seq_id] = 0
+        else:
+            stripped = list(line.strip())
+            g_count = stripped.count("G")
+            c_count = stripped.count("C")
+            gc_content = 100*(g_count+c_count)/len(stripped)
+            entries[seq_id] = gc_content
+
+    max_value = max(entries.values())
+    index = list(entries.values()).index(max_value)
+    print(entries)
+    print(list(entries.keys())[index]," ",max_value,"%")
+
+get_gc()
 
 f.close()
